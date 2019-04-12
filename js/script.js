d71a809d9f04047960693a44870abfe180bc46fd
Brent Carton <brent.carton@student.ehb.be>
2019-04-12 13:02:42 +0200

    seizoenen laten verschijnen en css toegevoegd

--- js/script.js
+++ js/script.js
@@ -17,13 +17,33 @@ $(function () {
         }).done(function (data) {
             console.log(data.results);
             for (var i = 0; i<10; i++){
-                $(".serieInfo").append("<h1>" + data.results[i].name + "</h1> <h3>" + data.results[i].first_air_date + "</h3> <p>" + data.results[i].overview + "</p>");
+                $(".serieInfo").append("<h1><a href='#'>" + data.results[i].name + "</a></h1> <h3>" + data.results[i].first_air_date + "</h3> <p>" + data.results[i].overview + "</p>");
             }
-            
+            $("h1").on("click", function () {
+                var seriePositie = ($(this).index())/3;
+                console.log(seriePositie);
+                $(".serieInfo").text("");
+                $(".serieInfo").append("<h1><a href='#'>" + data.results[seriePositie].name + "</a></h1>");
+                var serieId = data.results[seriePositie].id;
+                console.log(serieId);
+                $.ajax({
+                    url: "https://api.themoviedb.org/3/tv/" + serieId,
+                    method: "GET",
+                    data: ({
+                        api_key: "3fab369827f78498557f8e469f9910fb",
+                        tv_id:serieId
+                    })
+                }).done(function(data){
+                    for (var i = 0; i<10; i++){
+                        console.log(data)
+                        $(".serieInfo").append("<h2><a href='#'>" + data.seasons[i].name + "</a></h2><p>" + data.seasons[i].overview + "</p>");
+                    }
+                })
+            })
         }).fail(function (a,b) {
 
         }).always(function(){
             console.log("always");
-        }) 
-    })
-});
+        }); 
+    });
+})
