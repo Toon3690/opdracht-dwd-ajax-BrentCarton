b26ee7306bc9c2d529b8be6978c8e42efcebaa17
Brent Carton <brent.carton@student.ehb.be>
2019-04-18 11:51:58 +0200

    watchlist toegevoegd en css aangepast

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(function () {
     console.log('linked!');
     $("a").preventDefault;
+    var watchlist = false;
     //Zoekt de series
     $("input").on("keyup", function () {
         $(".serieInfo").text("");
@@ -17,7 +18,7 @@ $(function () {
             console.log(data.results);
             for (var i = 0; i<10; i++){
                 $(".serieInfo").append("<h1><a href='#/'>" + data.results[i].name + "</a></h1> <h3>" + data.results[i].first_air_date + "</h3> <p>" + data.results[i].overview + "</p>");
-            }
+            };
             //Toont de seizoenen
             $("h1").on("click", function () {
                 var seriePositie = ($(this).index())/3;
@@ -38,11 +39,13 @@ $(function () {
                         console.log(data)
                         $(".serieInfo").append("<h2 id='"+i+"'><a href='#/'>" + data.seasons[i].name + "</a></h2><p id='overview'>" + data.seasons[i].overview + "</p>");
                     };
+                    var serieNaam = data.name;
                     //Toont de episodes
                     $("h2").on("click", function () {
                         var seizoenPositie = (($(this).index())-1)/2;
                         console.log(seizoenPositie);
                         $("p#overview").hide();
+                        $(".episodeInfo").text("");
                         $.ajax({
                             url: "https://api.themoviedb.org/3/tv/" + serieId + "/season/" + seizoenPositie,
                             method: "GET",
@@ -54,8 +57,18 @@ $(function () {
                         }).done(function(data){
                             for (var i = 0; i<data.episodes.length; i++){
                                 console.log(data)
-                                $("#"+seizoenPositie+"").append("<div class'episodeInfo'><h4>"+data.episodes[i].name+"</h4><h5>"+data.episodes[i].air_date+"</h5><p>"+data.episodes[i].overview+"</p></div>");
+                                $("#"+seizoenPositie+"").append("<div class='episodeInfo'><h4><a href='#/'>"+data.episodes[i].name+"</a></h4><h5>"+data.episodes[i].air_date+"</h5><p>"+data.episodes[i].overview+"</p></div>");
                             };
+                            //Voegt episodes toe aan watchlist
+                            $("h4").on("click",function (){
+                                console.log(serieNaam);
+                                $(".watchlist").append("<h4>"+serieNaam+"</h4><h5>"+data.name+"</h5><p>"+$(this).text()+"</p>");
+                            });
+
+                        }).fail(function (a,b) {
+
+                        }).always(function(){
+                            console.log("always");
                         });
                     });
                 }).fail(function (a,b) {
@@ -69,5 +82,14 @@ $(function () {
         }).always(function(){
             console.log("always");
         }); 
+    });
+    $("#watchlist").on("mouseup",function(){
+        watchlist = !watchlist;
+        if(watchlist){
+            $(".watchlist").show();
+        }else{
+            $(".watchlist").hide();
+        };
+        console.log(watchlist);
     });
 })
